a29ef83d62a19f68f6889e3e4944a7d6c6b7dbbb
whoiam460 <whoiam@i.ua>
2021-11-12 16:31:10 +0200

    task3

--- src/compponents/taskThree.js
+++ src/compponents/taskThree.js
@@ -1,51 +1,49 @@
 import { useState } from 'react'
 
-
 import one from '../img/one.jpeg'
 import two from '../img/two.jpeg'
 import three from '../img/three.jpeg'
 
-
-
-const TaskThree = () =>{
-    const [img, setImg] = useState(1);
-    const images = [
-        {
-        img: one,
-        id:1
+const TaskThree = () => {
+  const [img, setImg] = useState(1)
+  const images = [
+    {
+      img: one,
+      id: 1,
     },
-         {
-        img: two,
-        id:2
+    {
+      img: two,
+      id: 2,
     },
-         {
-        img: three,
-        id:3
+    {
+      img: three,
+      id: 3,
     },
-]
-console.log();
+  ]
+  console.log()
 
-
-    return(
-        <div style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}>
+      <button
+        onClick={() => (img <= 1 ? setImg(images.length) : setImg(img - 1))}>
+        Prev
+      </button>
 
-          <button onClick = {()=> (img <= 1 ? setImg(images.length) : setImg(img-1))}>Prev</button>
+      {images.map(item => {
+        return item.id == img ? <img src={item.img} id={item.id} /> : null
+      })}
 
-       {images.map( (item) =>
-           {
-             return( (item.id == img) ? <img src ={item.img} id ={item.id}/> : null)
-            })
-        }
-
-      <button onClick = {()=> (img > images.length - 1 ? setImg(1) : setImg(img + 1))}>Next</button>
-        
-       </div>
-    )
+      <button
+        onClick={() => (img > images.length - 1 ? setImg(1) : setImg(img + 1))}>
+        Next
+      </button>
+    </div>
+  )
 }
-
 
 export default TaskThree
