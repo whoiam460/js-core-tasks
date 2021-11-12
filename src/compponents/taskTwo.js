cf61a132bd8cba1e4748532bf9bc9bb3f14c49e8
whoiam460 <whoiam@i.ua>
2021-11-12 16:29:00 +0200

    task2

--- src/compponents/taskTwo.js
+++ src/compponents/taskTwo.js
@@ -1,23 +1,18 @@
-import { useState } from "react"
-
-
+import { useState } from 'react'
 
 const TaskTwo = () => {
-    const[show, setShow] = useState('show')
-    return (
-        <div>
-            <button onClick = {  () =>{(show === 'hide')? setShow('show') : setShow('hide') }}>
-                {show}
-            </button>
-{
-
-
-            (show === 'hide')?
-            <p style = {{display:'block'}}>dlkfvjklsgfj</p> : ''
+  const [show, setShow] = useState('show')
+  return (
+    <div>
+      <button
+        onClick={() => {
+          show === 'hide' ? setShow('show') : setShow('hide')
+        }}>
+        {show}
+      </button>
+      {show === 'hide' ? <p style={{ display: 'block' }}>dlkfvjklsgfj</p> : ''}
+    </div>
+  )
 }
-        </div>
-    )
-}
-
 
 export default TaskTwo
